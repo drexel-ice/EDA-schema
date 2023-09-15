b28988a3e885bed623c0fb8f3a46799028a5dc9f
prtx <prtx.pratikshrestha@gmail.com>
2023-09-15 17:09:13 -0400

    Add schema gate, interconnect and timing path entities

--- eda_schema/entity.py
+++ eda_schema/entity.py
@@ -108,13 +108,158 @@ class CriticalPathMetricsEntity(BaseEntity):
             "properties": {
                 "startpoint": {"type": "string"},
                 "endpoint": {"type": "string"},
+                "path_type": {"type": "string"},
                 "worst_slack": {"type": "number"},
                 "no_of_slack_violations": {"type": "number"},
                 "worst_negative_slack": {"type": "number"},
                 "total_negative_slack": {"type": "number"},
+                "no_of_setup_violations": {"type": "number"},
+                "worst_setup_violation": {"type": "number"},
+                "total_setup_violation": {"type": "number"},
                 "no_of_hold_violations": {"type": "number"},
                 "worst_hold_violation": {"type": "number"},
                 "total_hold_violation": {"type": "number"},
+            },
+        },
+    }
+
+
+class GateEntity(BaseEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "gate"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "standard_cell": {"type": "string"},
+                "no_of_fanins": {"type": "number"},
+                "no_of_fanouts": {"type": "number"},
+                "x": {"type": ["number", "null"]},
+                "y": {"type": ["number", "null"]},
+            },
+        },
+    }
+
+
+class StandardCellEntity(BaseEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "standard_cell"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "width": {"type": "number"},
+                "height": {"type": "number"},
+                "no_of_input_pins": {"type": "number"},
+                "no_of_output_pins": {"type": "number"},
+                "is_sequential": {"type": "boolean"},
+                "is_inverter": {"type": "boolean"},
+                "is_buffer": {"type": "boolean"},
+                "drive_strength": {"type": ["number", "null"]},
+                "leakage_power": {"type": "number"},
+                "input_capacitance": {"type": "number"},
+                "output_capacitance": {"type": "number"},
+            },
+        },
+    }
+
+
+class InterconnectEntity(GraphEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "interconnect"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "no_of_inputs": {"type": "number"},
+                "no_of_outputs": {"type": "number"},
+                "x_min": {"type": ["number", "null"]},
+                "y_min": {"type": ["number", "null"]},
+                "x_max": {"type": ["number", "null"]},
+                "y_max": {"type": ["number", "null"]},
+                "hwpl": {"type": ["number", "null"]},
+                "rudy": {"type": ["number", "null"]},
+                "resistance": {"type": ["number", "null"]},
+                "capacitance": {"type": ["number", "null"]},
+            },
+        },
+    }
+
+
+class InterconnectSegmentEntity(BaseEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "standard_cell"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "length": {"type": ["number", "null"]},
+                "x1": {"type": ["number", "null"]},
+                "y2": {"type": ["number", "null"]},
+                "x2": {"type": ["number", "null"]},
+                "y2": {"type": ["number", "null"]},
+                "x": {"type": ["number", "null"]},
+                "y": {"type": ["number", "null"]},
+                "rudy": {"type": ["number", "null"]},
+                "resistance": {"type": ["number", "null"]},
+                "capacitance": {"type": ["number", "null"]},
+            },
+        },
+    }
+
+
+class TimingPathEntity(GraphEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "timing_path"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "startpoint": {"type": "string"},
+                "endpoint": {"type": "string"},
+                "path_type": {"type": "string"},
+                "arrival_time": {"type": "number"},
+                "required_time": {"type": "number"},
+                "slack": {"type": "number"},
+                "no_of_gates": {"type": "number"},
+                "is_critical_path": {"type": "boolean"},
+            },
+        },
+    }
+
+
+class TimingPointEntity(BaseEntity):
+    """Class for representing standard cell data using a JSON schema."""
+
+    title = "timing_point"
+    schema = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+                "cell_delay": {"type": "number"},
+                "arrival_time": {"type": "number"},
+                "node_depth": {"type": "number"},
             },
         },
     }
